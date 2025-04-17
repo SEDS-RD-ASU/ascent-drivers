3892e3350a3718f4019cb5332d6cd6172db023d1
Abdul Zia <abdulzia@asu.edu>
2025-04-16 18:42:49 -0700

    barometric altitude function

--- bmp390l/interface/interface_bmp390l.c
+++ bmp390l/interface/interface_bmp390l.c
@@ -9,34 +9,25 @@ void bmp390_set_ground_pressure(double pressure) {
     groundPressure = pressure; // Set the ground pressure
 }
 
-// Constants
-#define SEA_LEVEL_PRESSURE 1013.25  // hPa
-#define TEMP_LAPSE_RATE    -0.0065  // °C/m
-#define GAS_CONSTANT       287.05   // J/(kg·K)
-#define GRAVITY            9.80665  // m/s²
-#define METERS_TO_FEET     3.28084
+// Required constants (define based on ISA)
+#define SEA_LEVEL_PRESSURE 1013.25     // hPa
+#define TEMP_LAPSE_RATE 0.0065         // K/m
+#define GAS_CONSTANT 287.05            // J/(kg·K)
+#define GRAVITY 9.80665                // m/s^2
+#define METERS_TO_FEET 3.28084
 
-uint32_t bmp390_barometricAGL(void) {
-    double pressure, temperature;
+uint32_t bmp390_barometricAGL(double pressure_hPa, double groundPressure_hPa) {
+    if (pressure_hPa <= 0.0 || groundPressure_hPa <= 0.0) {
+        printf("Invalid pressure input: %.2f / %.2f\n", pressure_hPa, groundPressure_hPa);
+        return 0;
+    }
 
-    bmp390_read_sensor_data(&pressure, &temperature);
+    // ISA-based altitude (in meters)
+    double alt_measured = 44330.0 * (1.0 - pow(pressure_hPa / 1013.25, 1.0 / 5.255));
+    double alt_ground   = 44330.0 * (1.0 - pow(groundPressure_hPa / 1013.25, 1.0 / 5.255));
 
-    printf("Read from sensor P and T: %f, %f\n", pressure, temperature);
-    
-    // Convert to Kelvin
-    double temp_k = temperature + 273.15;
-
-    // Compute pressure altitude (in meters)
-    double pressure_alt = (temp_k / TEMP_LAPSE_RATE) * 
-                          (1.0 - pow(pressure / SEA_LEVEL_PRESSURE, 
-                                     (TEMP_LAPSE_RATE * GAS_CONSTANT) / -GRAVITY));
-
-    // Altitude Above Ground Level (in meters)
-    double agl_meters = pressure_alt - groundPressure;
+    double agl_meters = alt_measured - alt_ground;
     if (agl_meters < 0) agl_meters = 0;
 
-    // Convert to feet
-    double agl_feet = agl_meters * METERS_TO_FEET;
-
-    return (uint32_t)(agl_feet + 0.5); // Rounded to nearest foot
+    return (uint32_t)(agl_meters * METERS_TO_FEET + 0.5);  // return feet
 }
--- bmp390l/interface/interface_bmp390l.h
+++ bmp390l/interface/interface_bmp390l.h
@@ -7,5 +7,5 @@
 #include "driver_BMP390L.h"
 #include "i2c_manager.h"
 
-uint32_t bmp390_barometricAGL(void);
+uint32_t bmp390_barometricAGL(double pressure_hPa, double groundPressure_hPa);
 void bmp390_set_ground_pressure(double pressure);
