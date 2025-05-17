9008c770683a0f265fab20a1f2c85366e5c76a5b
Abdul Zia <abdulzia@asu.edu>
2025-05-17 17:56:32 -0400

    wipe GPS

--- sam_m10q/interface/interface_sam_m10q.h
+++ sam_m10q/interface/interface_sam_m10q.h
@@ -1,22 +1,4 @@
 #ifndef INTERFACE_SAM_M10Q_H
 #define INTERFACE_SAM_M10Q_H
 
-#include "driver_SAM_M10Q.h"
-
-esp_err_t gps_init();
-
-bool readUbxPort(ubxFrame *frame, int max_retries);
-
-void ubxDisableNMEA();
-
-bool ubxPollLocation();
-
-void NMEAstfu();
-
-void ubx10hz();
-
-void freeUbxFrame(ubxFrame *frame);
-
-void sendUbxFrame(ubxFrame frame);
-
 #endif /* INTERFACE_SAM_M10Q_H */
