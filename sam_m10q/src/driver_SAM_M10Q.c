50a83e0028c44773e937870de80fc93af741ffac
Abdul Zia <52299063+abdulziaa@users.noreply.github.com.>
2025-05-19 17:25:48 -0400

    temp commit

--- sam_m10q/src/driver_SAM_M10Q.c
+++ sam_m10q/src/driver_SAM_M10Q.c
@@ -13,4 +13,15 @@
 #include "driver_SAM_M10Q.h"
 
 #include "ascent_r2_hardware_definition.h"
+#include "i2c_manager.h"
+
+uint16_t ubxAvailableBytes()
+{   
+    uint16_t avail = 0;
+    uint8_t buf[2] = {0};
+    i2c_manager_read_register(I2C_MASTER_PORT, SAM_M10Q_I2C_ADDR, 0xFD, buf, 2);
+    avail = ((uint16_t)buf[1] << 8) | buf[0];
+
+    return avail;
+}
 
--- sam_m10q/src/driver_SAM_M10Q.h
+++ sam_m10q/src/driver_SAM_M10Q.h
@@ -1,4 +1,6 @@
 #ifndef DRIVER_SAM_M10Q_H
 #define DRIVER_SAM_M10Q_H
 
+uint16_t ubxAvailableBytes();
+
 #endif /* DRIVER_SAM_M10Q_H */
