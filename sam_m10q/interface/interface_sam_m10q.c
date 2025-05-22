580e0cac96c9dae6209554b4e23a95a7d11ec922
Abdul Zia <abdulzia@asu.edu>
2025-05-22 09:19:08 -0400

    added GPS interface

--- sam_m10q/interface/interface_sam_m10q.c
+++ sam_m10q/interface/interface_sam_m10q.c
@@ -14,3 +14,61 @@
 
 #include "ascent_r2_hardware_definition.h"
 #include "i2c_manager.h"
+#include "driver_SAM_M10Q.h"
+
+void GPS_init(void) {
+    ubxResetGPS();
+
+    printf("Reset GPS!\n\n");
+
+    printf("Disabling NMEA...\n\n");
+
+    ubxDisableNMEA();
+
+    ubxReadStream(NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+    printf("Setting 10hz...\n\n");
+
+    ubx10HzGPS();
+
+    ubxReadStream(NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+    printf("Setting constellations to GPS only...\n\n");
+
+    ubxConstellations();
+
+    ubxReadStream(NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+    printf("Setting 20hz for timepulse...\n\n");
+
+    ubxFreezeTimePulse();
+
+    ubxReadStream(NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+    printf("Enabling NAV_PVT messages...\n\n");
+
+    ubxEnableNavPVT();
+
+    ubxReadStream(NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+    printf("Setting rate for NAV_PVT messages...\n\n");
+
+    ubxMsgOutCfg();
+
+    ubxReadStream(NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+}
+
+void GPS_read(uint32_t *UTCtstamp, int32_t *lon, int32_t *lat, int32_t *height, int32_t *hMSL, uint8_t *fixType, uint8_t *numSV) {
+    ubxReadStream(UTCtstamp, lon, lat, height, hMSL, fixType, numSV);
+}
+
+void GPS_timing_debug(void) {
+    ubxReadStreamTiming();
+}
+
+void GPS_reset(void) {
+    ubxReadStream(NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+    ubxResetGPS();
+    ubxReadStream(NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+    printf("Reset GPS!\n\n");
+}
--- sam_m10q/interface/interface_sam_m10q.h
+++ sam_m10q/interface/interface_sam_m10q.h
@@ -1,4 +1,14 @@
 #ifndef INTERFACE_SAM_M10Q_H
 #define INTERFACE_SAM_M10Q_H
 
+#include "driver_SAM_M10Q.h"
+
+void GPS_init(void);
+
+void GPS_read(uint32_t *UTCtstamp, int32_t *lon, int32_t *lat, int32_t *height, int32_t *hMSL, uint8_t *fixType, uint8_t *numSV);
+
+void GPS_timing_debug(void);
+
+void GPS_reset(void);
+
 #endif /* INTERFACE_SAM_M10Q_H */
