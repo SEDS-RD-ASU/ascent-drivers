b083f19f3c1dcd6b256678933d9da9007894a11f
Abdul Zia <abdulzia@asu.edu>
2025-09-06 18:59:36 -0700

    Revert "nuked the driver"
    This reverts commit 0ef2b0cb8854c9939029a7c428f607a24d418d3e.

--- sam_m10q/src/driver_SAM_M10Q.h
+++ sam_m10q/src/driver_SAM_M10Q.h
@@ -1,5 +1,34 @@
 #ifndef DRIVER_SAM_M10Q_H
 #define DRIVER_SAM_M10Q_H
 
+uint16_t ubxAvailableBytes();
+
+void ubxReadBytes(uint8_t *buf, uint16_t num_bytes);
+
+void ubxDisableNMEA();
+
+void ubxEnableNavPVT();
+
+void ubxReadStream(uint32_t *UTCtstamp, int32_t *lon, int32_t *lat, int32_t *height, int32_t *hMSL, uint8_t *fixType, uint8_t *numSV);
+
+void ubxReadStreamTiming();
+
+void ubxResetGPS();
+
+void ubx10HzGPS();
+
+// void ubx25hzGPS(); // DOO NOOTTT USE UNLESS YOU KNOW WHAT YOURE DOING.
+
+void ubxConstellations();
+
+void ubxMsgOutCfg();
+
+void ubxFreezeTimePulse();
+
+void ubxDisableBDS_B1();
+
+void ubxLowPowerMode();
+
+void ubxNormalPowerMode();
 
 #endif /* DRIVER_SAM_M10Q_H */
