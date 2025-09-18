8efd6860c10edffec86aa0ad801d7386a01493d6
Abdul Zia <abdulzia@asu.edu>
2025-09-17 21:52:39 -0700

    one last commit (frfr)

--- sam_m10q/interface/interface_sam_m10q.c
+++ sam_m10q/interface/interface_sam_m10q.c
@@ -46,7 +46,7 @@ void GPS_init(void) {
 }
 
 
-void GPS_ReqNavPVT(uint32_t *UTCtstamp, int32_t *lon, int32_t *lat, int32_t *height, int32_t *hMSL, uint8_t *fixType, uint8_t *numSV) {
+void GPS_read(uint32_t *UTCtstamp, int32_t *lon, int32_t *lat, int32_t *height, int32_t *hMSL, uint8_t *fixType, uint8_t *numSV) {
     esp_err_t ret;
     sam_m10q_msginfo_t msginfo;
     uint8_t gps_packet_buf[GPS_MAX_PACKET_SIZE];
--- sam_m10q/interface/interface_sam_m10q.h
+++ sam_m10q/interface/interface_sam_m10q.h
@@ -3,7 +3,7 @@
 
 #include "driver_SAM_M10Q.h"
 
-void GPS_ReqNavPVT(uint32_t *UTCtstamp, int32_t *lon, int32_t *lat, int32_t *height, int32_t *hMSL, uint8_t *fixType, uint8_t *numSV);
+void GPS_read(uint32_t *UTCtstamp, int32_t *lon, int32_t *lat, int32_t *height, int32_t *hMSL, uint8_t *fixType, uint8_t *numSV);
 
 void GPS_init(void);
 
